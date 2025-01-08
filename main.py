aabb093d91d66e618f32e9dd0efaed6cf81634b3
Gauri Musmade <103319755+Gaurimusmade@users.noreply.github.com>
2025-01-08 14:22:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-
-options = Options()
-options.binary_location = "C:\\path\\to\\chrome.exe"    #chrome binary location specified here
-options.add_argument("--start-maximized") #open Browser in maximized mode
-options.add_argument("--no-sandbox") #bypass OS security model
-options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(options=options, executable_path=r'C:\path\to\chromedriver.exe')
+driver = webdriver.Chrome()
 
 
 # Initialize WebDriver
