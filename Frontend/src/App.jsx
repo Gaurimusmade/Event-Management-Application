685582f993be0d42546f957e904e1f7bfba5e6b4
Gauri Musmade <103319755+Gaurimusmade@users.noreply.github.com>
2025-01-13 18:28:39 +0530

    Hello world added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     <>
       <Navbar />
       <h1>Hello Gauriiiiiiiiiii</h1>
+      <h1>Hello World</h1>
       <Routes>
         <Route path="/" element={<EventDashboard />} />
         <Route path="/eventlist" element={<AllEvent />} />
